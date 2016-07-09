6563f91a2a32bb5eb2aabc73b008464534eaba22
Thiago lopes da silva <thiagoolsilva@gmail.com>
2016-07-09 01:45:55 -0300

    [master] - initial commit

---
+++ code/StarterProject/app/src/main/java/lopes/br/starterproject/communication/Request.java
@@ -0,0 +1,59 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package lopes.br.starterproject.communication;
+
+import com.squareup.okhttp.OkHttpClient;
+
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+
+/**
+ * The type Request.
+ */
+public class Request {
+
+    //1.
+    //The URL of an APK of production will be http://www.mocky.io/v2/57806d2e1000003c1f62fd31
+    //2.
+    //The URL of an APK of developed will be http://www.mocky.io/v2/57806d521000002b1f62fd33
+
+    //NOTE: Using the retrofit.You can use the @Path instead of flavor. On this moment, only for study we will use the flavor strategy.
+    //NOTE: If you want more details about retrofit. Check this link http://square.github.io/retrofit/
+
+    private final String API_BASE_URL = "";
+
+    private static OkHttpClient client = new OkHttpClient();
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(API_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+
+    /**
+     * Create requests api request.
+     *
+     * @return the api request
+     */
+    public static ApiRequest createRequests() {
+        Retrofit retrofit = builder.client(client)
+                .build();
+        return retrofit.create(ApiRequest.class);
+    }
+}
---
+++ code/StarterProject/app/src/main/java/lopes/br/starterproject/controller/CommunicationController.java
@@ -0,0 +1,79 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package lopes.br.starterproject.controller;
+
+import android.support.annotation.WorkerThread;
+import android.util.Log;
+
+import java.io.IOException;
+
+import lopes.br.starterproject.communication.ApiRequest;
+import lopes.br.starterproject.communication.Request;
+import lopes.br.starterproject.communication.model.MessageResponse;
+import retrofit.Response;
+
+/**
+ * The type Communication controller.
+ */
+public class CommunicationController {
+
+    private static final String TAG = "request";
+
+    private static CommunicationController sInstance;
+
+    private static Object lockObj = new Object();
+
+    private CommunicationController() {}
+
+    /**
+     * Gets instance.
+     *
+     * @return the instance
+     */
+    public static CommunicationController getsInstance() {
+        synchronized (lockObj) {
+            if (sInstance == null) {
+                sInstance = new CommunicationController();
+            }
+            return sInstance;
+        }
+    }
+
+    /**
+     * Get message from web server
+     *
+     * @return The message received
+     */
+    @WorkerThread
+    public  String GetMessageFromWebServer() {
+        String responseMessage = "";
+        ApiRequest apiRequest = Request.createRequests();
+
+        try {
+            Response<MessageResponse> request = apiRequest.GETMessage().execute();
+            if(request != null && request.body() != null) {
+                MessageResponse bodyRequest = request.body();
+                responseMessage = bodyRequest.getMessage();
+            }
+        } catch (IOException e) {
+            Log.d(TAG, "error on request:: "+e.getMessage());
+        }
+        return responseMessage;
+    }
+}
